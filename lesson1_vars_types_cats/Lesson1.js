025050fcaeb04cc5b45931ee924516bf478f805c
LimitlessInfinite <1218bogdan@gmail.com>
2024-02-12 07:52:31 +0200

    Completed the first task

--- lesson1_vars_types_cats/Lesson1.js
+++ lesson1_vars_types_cats/Lesson1.js
@@ -1,1 +1,56 @@
+/*
+- Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com',
+'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+Вивести кожну змінну за допомогою: console.log
+*/
+const greeting = "hello";
+const site = "owu";
+const tld = ".com";
+const tld2 = "ua";
+
+const number1 = 1;
+const number2 = 10;
+const number3 = -999;
+const number4 = 123;
+const floatNumber = 3.14;
+const floatNumber2 = 2.7;
+const number5 = 16;
+
+const boolType1 = true;
+const boolType2 = false;
+
+let testArray = [];
+testArray.push(greeting,site,tld,tld2,number1,number2,number3,number4,floatNumber,floatNumber2,number5,boolType1,boolType2)
+
+console.log(greeting)
+console.log(site)
+console.log(tld)
+console.log(tld2)
+console.log(number1)
+console.log(number2)
+console.log(number3)
+console.log(number4)
+console.log(floatNumber)
+console.log(floatNumber2)
+console.log(number5)
+console.log(boolType1)
+console.log(boolType2)
+console.log("-".repeat(20))
+
+console.log(testArray)
+
+console.log("Length of test array = " + testArray.length)
+/*
+
+- Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ.
+З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+- За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+  let a = 100; let b = '100'; let c = true;
+
+Додаткове для тих хто цікавився prompt`oм
+- За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям,
+По-Батькові та роками. та вивести в консоль
+ */
+
 console.log("Hello world, test!")
