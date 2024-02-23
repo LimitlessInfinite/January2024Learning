09ee94956b8d8867340c4a24793589beaa557211
LimitlessInfinite <1218bogdan@gmail.com>
2024-02-23 08:26:49 +0200

    Complete task 8.5 in Lesson2

--- lesson2_arrays_objects_if/Lesson2.js
+++ lesson2_arrays_objects_if/Lesson2.js
@@ -88,14 +88,11 @@ const day = 1;
 
 if (day >= 1 && day <= 10) {
     console.log("1 decade");
-}
-else if (day >= 11 && day <= 20) {
+} else if (day >= 11 && day <= 20) {
     console.log("2 decade");
-}
-else if (day >= 21 && day <= 31) {
+} else if (day >= 21 && day <= 31) {
     console.log("3 decade");
-}
-else console.log("Sorry, the day value is wrong")
+} else console.log("Sorry, the day value is wrong")
 /*
 8) - Скласти розклад на тиждень за домопоги switch.
 
@@ -128,7 +125,8 @@ switch (dayOfWeek) {
     case "7":
         alert("Pre-competition conditioning check");
         break;
-    default: alert("Enjoy life =)");
+    default:
+        alert("Enjoy life =)");
 }
 
 /*
@@ -142,14 +140,11 @@ let number1 = prompt("Enter the first number")
 let number2 = prompt("Enter the second number")
 if (!Number.isInteger(parseInt(number1)) || !Number.isInteger(parseInt(number2))) {
     alert("Incorrect format, enter only integers")
-}
-else if (parseInt(number1) > parseInt(number2)) {
+} else if (parseInt(number1) > parseInt(number2)) {
     alert("First number (" + number1 + ") is bigger than second number (" + number2 + ")")
-}
-else if (parseInt(number2) > parseInt(number1)) {
+} else if (parseInt(number2) > parseInt(number1)) {
     alert("Second number (" + number2 + ") is bigger than first number (" + number1 + ")")
-}
-else if (parseInt(number2) === parseInt(number1)) {
+} else if (parseInt(number2) === parseInt(number1)) {
     alert("Numbers equals")
 }
 
@@ -166,9 +161,7 @@ let y = "true";
 
 if (y === false || y === null || y === undefined || y === 0 || y === -1 || y === 0n || isNaN(y) || y === "") {
     alert("y value is falsy")
-}
-else alert("y value is true")
-
+} else alert("y value is true")
 
 
 /*
@@ -192,3 +185,78 @@ for (let i = 0; i < coursesAndDurationArray.length; i++) {
         alert(coursesAndDurationArray[i].title + " super!")
     }
 }
+
+/*
+8.5 - Нам дано масив друзів let friends = ["Ivan", "Slavik", "Grisha", "Lera", "Ira"];
+Потрібно зробити перевірку якщо кількість елементів у масиві більше або дорівнює 3,
+ то виводиться повідомлення про те, що це великий масив, в якому  3 і більше елементи.
+    Інакше слід вивести на екран повідомлення про те, що це маленький масив,
+    в якому менше 3-х елементів.
+*/
+
+let friends = ["Ivan", "Slavik", "Grisha", "Lera", "Ira"];
+
+if (friends.length >= 3) {
+    alert("Is big array")
+}
+else alert("Is small array")
+
+
+/*
+- Маємо 3 числа і між ним  нам потрібно знайти те що посередині.
+    Знайти, яке з них є середнім (більше одного, але менше за інше). Перевірити, чи знаходиться перше число між двома іншими.
+*/
+
+/*
+- Перепишіть конструкцію if з використанням умовного оператора '?':
+let result;
+if (a + b < 4) {
+    result = 'Мало';
+} else {
+    result = 'Багато';
+}
+*/
+
+
+/*
+- Маємо будь яке число від -100 , 0 , +100 потрібно перевірити яким є число: позитивним, негативним або нулем
+напишіть це тернарним оператором
+
+*/
+
+
+/*
+- У нас є змінна test, якщо дорівнює true, виведіть 'Вірно', інакше виведіть 'Неправильно'.
+    Перевірте роботу скрипта при test, що дорівнює true, false. Напишіть два варіанти скрипта - з коротким записом(тернаркою)
+*/
+
+/*
+- Використовуючи конструкцію if..else, напишіть код, який запитуватиме: „Яка «офіційна» назва JavaScript?“
+Якщо користувач вводить «ECMAScript», то показати: «Правильно!», інакше – відобразити: «Не знаєте? ECMAScript!
+*/
+
+
+/*
+    - У першому під'їзді квартири з 1 до 20. У другому з 21 по 48. У третьому з 49 по 90.
+Користувач вводить номер квартири просто в змінні або через prompt('') .
+    Програма повинна зазначити, в якому під'їзді знаходиться дана квартира.
+*/
+
+
+/*
+- Ми маємо змінну number в яку користувач задає числове значення,  якщо змінна рівна 10 вивести повідомлення ВІРНО, якщо змінна не рівна 10 - тоді НЕВІРНО
+*/
+
+
+/*
+- Задача-гра "чи ми йдемо сьогодні в OKTEN на навчання?".
+    Змінна, яка характеризує температуру. Якщо температура від +10 до +22 вивести повідомлення що ми йдемо ВЧИТИСЯ .
+    Якщо інша темпаретура, тоді виводимо повідомлення що сидимо вдома і вчимося ОНЛАЙН
+*/
+
+
+/*
+- За допомогою switch case - реалізувати гру - вікторину, де ми вводимо число від 1 до 5. І взалежності від введеного
+    числа нам дається приз. (Авто, Мото, Телефон, і т.д ) і якщо число не підходить тоді  вивести Повідомлення - що число не вірне ... .
+*/
+
