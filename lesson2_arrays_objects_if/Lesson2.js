7601f00d2852c50883b467d01dda20c05c4cef63
LimitlessInfinite <1218bogdan@gmail.com>
2024-02-23 08:08:52 +0200

    Complete task 8.4 in Lesson2

--- lesson2_arrays_objects_if/Lesson2.js
+++ lesson2_arrays_objects_if/Lesson2.js
@@ -177,3 +177,18 @@ else alert("y value is true")
 У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".
 
  */
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+for (let i = 0; i < coursesAndDurationArray.length; i++) {
+    if (coursesAndDurationArray[i].monthDuration > 5) {
+        alert(coursesAndDurationArray[i].title + " super!")
+    }
+}
