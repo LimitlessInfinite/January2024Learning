d75dca8d9d494950fb3919a342f163a51164fd98
LimitlessInfinite <1218bogdan@gmail.com>
2024-03-10 08:21:44 +0200

    Complete task 8 in Lesson4

--- lesson4_functions/Lesson4.js
+++ lesson4_functions/Lesson4.js
@@ -67,7 +67,20 @@ function ulTripleLi (text,count) {
 
 ulTripleLi("Hello",3)
 // 8 - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
-// - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
+document.write('<div class="separator">Task 8</div>')
+
+let arrayPrimitives = [1,"Test",true,2,"Desktop",false,3,"Linux"]
+
+function listPrimitives (array) {
+    document.write('<div><ul>')
+    for (let i = 0; i < array.length; i++) {
+        document.write('<li>'+ array[i] +'</li>')
+    }
+    document.write('</ul></div>')
+}
+
+listPrimitives(arrayPrimitives)
+// 9 - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
 // - створити функцію яка повертає найменьше число з масиву
 // - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
 // - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
